5b680ff2357563fbda1de841af53b2a6aa097cfa
Agustin Kassis <webmaster@masize.com>
2023-04-08 13:38:09 -0300

    added nostr extension type

---
+++ src/types/nostr.ts
@@ -0,0 +1,19 @@
+import type { Event } from "./event";
+
+export default interface NostrExtensionProvider {
+  nip04: Nip04;
+  enabled: boolean;
+
+  enable: () => Promise<{ enabled: boolean }>;
+  getPublicKey: () => Promise<string>;
+  signEvent: (event: Event) => Promise<Event>;
+  signSchnorr: (sigHash: string) => Promise<Buffer>;
+  getRelays: () => Promise<string[]>;
+  execute: <T>(action: string, args?: Record<string, unknown>) => Promise<T>;
+}
+
+interface Nip04 {
+  provider: NostrExtensionProvider;
+  encrypt: (peer: string, plaintext: string) => Promise<string>;
+  decrypt: (peer: string, ciphertext: string) => Promise<string>;
+}
