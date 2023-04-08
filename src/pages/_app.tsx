1001454c8e4140b37ae613498be0997ec704a588
Agustin Kassis <webmaster@masize.com>
2023-04-08 13:42:46 -0300

    added nostr provider wrapper

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
 import { type AppType } from "next/dist/shared/lib/utils";
+import { NostrProvider } from "~/contexts/nostr";
 
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <NostrProvider>
+      <Component {...pageProps} />
+    </NostrProvider>
+  );
 };
 
 export default MyApp;
