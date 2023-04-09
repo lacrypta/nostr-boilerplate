58cca1a23f3b52b0907fd7c2b962e30118c2e0f8
Agustin Kassis <webmaster@masize.com>
2023-04-09 19:55:33 -0300

    removed profile from connect

--- src/components/main/connected.tsx
+++ src/components/main/connected.tsx
@@ -1,19 +1,27 @@
 import useNOSTR from "~/hooks/useNOSTR";
 import Button from "../button";
-import Profile from "../profile";
+import { useRouter } from "next/router";
+import { nip19 } from "nostr-tools";
+import { useCallback } from "react";
 
 export const Connected = () => {
   const { pubKey } = useNOSTR();
+  const router = useRouter();
+
+  const goToProfile = useCallback(() => {
+    const npub = nip19.npubEncode(pubKey as string);
+    void router.push(`/${npub}`);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [pubKey]);
+
   if (!pubKey) {
     return <div>Nothing</div>;
   }
+
   return (
     <div className="space-y-4 text-center">
       <div className="flex flex-col content-center justify-center">
-        <Profile pubKey={pubKey} />
-        <Button onClick={() => alert("Estamos laburando. No seas ansiosa")}>
-          Ohh yeahhh
-        </Button>
+        <Button onClick={() => void goToProfile()}>Ver Perfil</Button>
       </div>
     </div>
   );
