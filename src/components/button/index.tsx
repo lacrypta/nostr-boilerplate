d76fb0a399fbf1bc9e303a7f49e082703201a724
Agustin Kassis <webmaster@masize.com>
2023-04-09 19:19:06 -0300

    wrapp button with className

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -3,16 +3,19 @@ import type { MouseEventHandler } from "react";
 export interface ButtonProps {
   onClick?: MouseEventHandler | undefined;
   children: React.ReactNode;
+  className?: string;
 }
 
-export const Button = ({ children, onClick }: ButtonProps) => {
+export const Button = ({ children, className, onClick }: ButtonProps) => {
   return (
-    <button
-      className="flex flex-col rounded-xl bg-white/10 p-5 text-lg text-white transition-transform ease-in-out hover:bg-white/20 active:scale-95 active:bg-white/5"
-      onClick={onClick}
-    >
-      {children}
-    </button>
+    <div className={className}>
+      <button
+        className="flex flex-col rounded-xl bg-white/10 p-5 text-lg text-white transition-transform ease-in-out hover:bg-white/20 active:scale-95 active:bg-white/5"
+        onClick={onClick}
+      >
+        {children}
+      </button>
+    </div>
   );
 };
 
