beb5eca08c534ef849e40c9b9396a22f89460763
Agustin Kassis <webmaster@masize.com>
2023-04-09 19:55:12 -0300

    webln must be enabled on connection

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,9 +6,11 @@ import Connected from "~/components/main/connected";
 import NotConnected from "~/components/main/notConnected";
 import Title from "~/components/title";
 import useNOSTR from "~/hooks/useNOSTR";
+import useWebLN from "~/hooks/useWebLN";
 
 const Home: NextPage = () => {
   const { pubKey } = useNOSTR();
+  const { isEnabled } = useWebLN();
 
   return (
     <>
@@ -20,7 +22,7 @@ const Home: NextPage = () => {
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#15162c] to-[#2e026d] text-2xl text-white">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <Title />
-          {pubKey ? <Connected /> : <NotConnected />}
+          {isEnabled && pubKey ? <Connected /> : <NotConnected />}
         </div>
       </main>
       <Footer />
