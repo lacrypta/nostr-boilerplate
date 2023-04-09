dab5f9a9a5fff7673ad2c1c6f86d4a19c2c7e5a6
Agustin Kassis <webmaster@masize.com>
2023-04-09 16:45:26 -0300

    wrapped NostrApp inside NostrProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,15 +2,21 @@ import { type AppType } from "next/dist/shared/lib/utils";
 import { NostrAccountProvider } from "~/contexts/nostr";
 import { WebLNProvider } from "~/contexts/webln";
 
+import { NostrProvider } from "nostr-react";
+
 import "~/styles/globals.css";
+
+const relayUrls = ["wss://nostr-pub.wellorder.net", "wss://relay.nostr.ch"];
 
 const NostrApp: AppType = ({ Component, pageProps }) => {
   return (
+    <NostrProvider relayUrls={relayUrls}>
       <WebLNProvider>
         <NostrAccountProvider>
           <Component {...pageProps} />
         </NostrAccountProvider>
       </WebLNProvider>
+    </NostrProvider>
   );
 };
 
