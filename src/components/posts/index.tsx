1a55906507edd6272f2cd868a387d5bd4c755d88
Agustin Kassis <webmaster@masize.com>
2023-04-10 21:51:14 -0300

    comment out posts

--- src/components/posts/index.tsx
+++ src/components/posts/index.tsx
@@ -1,21 +1,19 @@
-import { useNostrEvents } from "nostr-react";
-
 interface PostsProps {
   pubKey: string;
 }
 
 export const Posts = ({ pubKey }: PostsProps) => {
-  const { events } = useNostrEvents({
-    filter: {
-      authors: [pubKey],
-      // since: dateToUnix(now.current), // all new events from now
-      kinds: [1],
-    },
-  });
+  // const { events } = useNostrEvents({
+  //   filter: {
+  //     authors: [pubKey],
+  //     // since: dateToUnix(now.current), // all new events from now
+  //     kinds: [1],
+  //   },
+  // });
 
-  const sorted = events.sort((a, b) => b.created_at - a.created_at);
+  // const sorted = events.sort((a, b) => b.created_at - a.created_at);
 
-  return <div>{JSON.stringify(sorted)}</div>;
+  return <div>{JSON.stringify({ pubKey })}</div>;
 };
 
 export default Posts;
