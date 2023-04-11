e03b72588e8105bba851cbd7b9110ac2d421f0ce
Agustin Kassis <webmaster@masize.com>
2023-04-10 21:52:01 -0300

    added profile type

---
+++ src/types/profile.ts
@@ -0,0 +1,13 @@
+export interface Profile {
+  npub: string;
+  name?: string | undefined;
+  username?: string | undefined;
+  display_name?: string | undefined;
+  picture?: string | undefined;
+  banner?: string | undefined;
+  about?: string | undefined;
+  website?: string | undefined;
+  lud06?: string | undefined;
+  lud16?: string | undefined;
+  nip05?: string | undefined;
+}
