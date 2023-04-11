2aa1408a7cb1d16fdab9b7fb5e93873a4d5451bb
Agustin Kassis <webmaster@masize.com>
2023-04-10 22:24:03 -0300

    added multiple relays

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,16 @@ import { WebLNProvider } from "~/contexts/webln";
 import "~/styles/globals.css";
 import { NostrRelayProvider } from "~/contexts/nostrRelay";
 
-const relayUrls = ["wss://nostr-pub.wellorder.net"];
+const relayUrls = [
+  "wss://nostr1.tunnelsats.com",
+  "wss://nostr-01.bolt.observer",
+  "wss://nostr-pub.wellorder.net",
+  "wss://nostr-relay.wlvs.space",
+  "wss://nostr.bitcoiner.social",
+  "wss://relay.damus.io",
+  "wss://relay.nostr.info",
+  "wss://relayer.fiatjaf.com",
+];
 
 const NostrApp: AppType = ({ Component, pageProps }) => {
   return (
