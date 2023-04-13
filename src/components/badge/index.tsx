296e749a1d86e8e5dea8e462e3ba674e2869e428
Agustin Kassis <webmaster@masize.com>
2023-04-13 17:58:24 -0300

    added alert on invalid badge

--- src/components/badge/index.tsx
+++ src/components/badge/index.tsx
@@ -6,14 +6,24 @@ interface BadgeProps {
 }
 
 export const Badge = ({ preBadge }: BadgeProps) => {
-  const { badge, isLoading } = useBadge({ preBadge });
+  const { badge, isLoading, isValid } = useBadge({ preBadge });
   return (
-    <div className="inline-block h-10 w-10 overflow-hidden rounded-full bg-white/10 text-black transition hover:scale-125">
-      {isLoading ? (
-        ""
-      ) : (
-        // eslint-disable-next-line @next/next/no-img-element
-        <img alt={badge.definition.description} src={badge.definition.image} />
+    <div className="relative inline-block">
+      <div className="h-10 w-10 overflow-hidden rounded-full bg-white/10 text-black transition hover:scale-125">
+        {isLoading ? (
+          ""
+        ) : (
+          // eslint-disable-next-line @next/next/no-img-element
+          <img
+            alt={badge.definition.description}
+            src={badge.definition.image}
+          />
+        )}
+      </div>
+      {isValid === false && (
+        <div className="absolute -right-1 -top-1 h-5 w-5 rounded-full bg-red-600 text-sm">
+          !
+        </div>
       )}
     </div>
   );
