f572964fc919f96c91985caf857245bcd73cef48
Agustin Kassis <webmaster@masize.com>
2023-04-13 17:47:06 -0300

    aesthetic badge background

--- src/components/badge/index.tsx
+++ src/components/badge/index.tsx
@@ -7,12 +7,10 @@ interface BadgeProps {
 
 export const Badge = ({ preBadge }: BadgeProps) => {
   const { badge, isLoading } = useBadge({ preBadge });
-  // console.info("!!!! Badge !!!!");
-  // console.dir(badge);
   return (
-    <div className="inline-block h-10 w-10 cursor-pointer overflow-hidden rounded-full bg-white text-black transition hover:scale-125">
+    <div className="inline-block h-10 w-10 cursor-pointer overflow-hidden rounded-full bg-white/10 text-black transition hover:scale-125">
       {isLoading ? (
-        "L"
+        ""
       ) : (
         // eslint-disable-next-line @next/next/no-img-element
         <img alt={badge.definition.description} src={badge.definition.image} />
