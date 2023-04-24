2afcc2365ef60fb14b20e5ac4c00912f36a0f21c
Agustin Kassis <webmaster@masize.com>
2023-04-24 11:19:35 -0300

    removed unused BsArrowRight icon

--- src/components/main/notConnected.tsx
+++ src/components/main/notConnected.tsx
@@ -1,18 +1,17 @@
 import useNOSTR from "~/hooks/useNOSTR";
 import Button from "../button";
-import { BsArrowRight } from "react-icons/bs";
 
 export const NotConnected = () => {
   const { login } = useNOSTR();
   return (
     <div className="flex justify-evenly lg:w-4/5">
-      <div className="text-center m-4">
+      <div className="m-4 text-center">
         {/* <h4 className="text-lg mb-8">¿Es la primera vez por acá?</h4> */}
         <Button onClick={() => window.open("https://getalby.com/", "_blank")}>
           Descarga una wallet
         </Button>
       </div>
-      <div className="text-center m-4">
+      <div className="m-4 text-center">
         {/* <h4 className="text-lg mb-8">Si tienes un usuario y una wallet instalada</h4> */}
         <Button
           onClick={() => {
